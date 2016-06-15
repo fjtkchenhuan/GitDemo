b29841992f26461bde5b553248cfef01eff3b257
chenhuan <13714854975@163.com>
2016-06-15 10:04:39 +0800

    第一次提交

--- app/src/main/java/com/example/fjtk/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/fjtk/gitdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.fjtk.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
+
+    private TextView viewById;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewById = (TextView) findViewById(R.id.tv);
     }
 }
